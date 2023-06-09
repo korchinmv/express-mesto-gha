c1e080b1f259da9935b637d6a811823e54646786
korchinmv <korchinmv@yandex.ru>
2023-06-09 07:28:10 +0300

    autotest fix 2

--- routes/users.js
+++ routes/users.js
@@ -13,9 +13,9 @@ const {
 
 router.get('/', auth, getUsers);
 router.get('/me', auth, getUser);
-router.get('/:userId', validateFindById, auth, getUserById);
-router.patch('/me', validateUpdateUser, auth, updateUser);
-router.patch('/me/avatar', validateUpdateAvatar, auth, updateAvatar);
+router.get('/:userId', auth, validateFindById, getUserById);
+router.patch('/me', auth, validateUpdateUser, updateUser);
+router.patch('/me/avatar', auth, validateUpdateAvatar, updateAvatar);
 router.post('/signin', validateSignIn, login);
 router.post('/signup', validateSignUp, createUser);
 
